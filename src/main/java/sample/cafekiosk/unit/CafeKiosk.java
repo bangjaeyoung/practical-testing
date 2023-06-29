fdefe536c3e1bc5943529a8fe817614b1ffa3fa4
bangjaeyoung <qkdwo4613@gmail.com>
2023-06-30 01:15:53 +0900

    테스트하기 어려운 영역 분리 및 테스트 케이스 세분화

--- src/main/java/sample/cafekiosk/unit/CafeKiosk.java
+++ src/main/java/sample/cafekiosk/unit/CafeKiosk.java
@@ -5,16 +5,30 @@ import sample.cafekiosk.unit.beverage.Beverage;
 import sample.cafekiosk.unit.order.Order;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 public class CafeKiosk {
 
+    private static final LocalTime SHOP_OPEN_TIME = LocalTime.of(10, 0);
+    private static final LocalTime SHOP_CLOSE_TIME = LocalTime.of(22, 0);
+
     private final List<Beverage> beverages = new ArrayList<>();
 
     public void add(Beverage beverage) {
         beverages.add(beverage);
+    }
+
+    public void add(Beverage beverage, int count) {
+        if (count <= 0) {
+            throw new IllegalArgumentException("음료는 1잔 이상 주문하실 수 있습니다.");
+        }
+
+        for (int i = 0; i < count; i++) {
+            beverages.add(beverage);
+        }
     }
 
     public void remove(Beverage beverage) {
@@ -33,7 +47,26 @@ public class CafeKiosk {
         return totalPrice;
     }
 
-    public Order createOrder() {
+//    public Order createOrder() {
+//        LocalDateTime currentDateTime = LocalDateTime.now();
+//        LocalTime currentTime = currentDateTime.toLocalTime();
+//
+//        if (currentTime.isBefore(SHOP_OPEN_TIME) || currentTime.isAfter(SHOP_CLOSE_TIME)) {
+//            throw new IllegalArgumentException("주문 시간이 아닙니다. 관리자에게 문의하세요.");
+//        }
+//
+//        return new Order(LocalDateTime.now(), beverages);
+//    }
+
+    // 테스트하기 어려운 영역을 구분하고 분리(LocalDateTime.now(): 현재 시각(수행할 때마다 달라지기 때문))
+    public Order createOrder(LocalDateTime currentDateTime) {
+//        LocalDateTime currentDateTime = LocalDateTime.now();
+        LocalTime currentTime = currentDateTime.toLocalTime();
+
+        if (currentTime.isBefore(SHOP_OPEN_TIME) || currentTime.isAfter(SHOP_CLOSE_TIME)) {
+            throw new IllegalArgumentException("주문 시간이 아닙니다. 관리자에게 문의하세요.");
+        }
+
         return new Order(LocalDateTime.now(), beverages);
     }
 }
