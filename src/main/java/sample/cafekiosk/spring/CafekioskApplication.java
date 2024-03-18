ff967731e4fe61d9bf91269ddb0cd8957024452d
Jaeyoung Bang <qkdwo4613@gmail.com>
2024-03-18 23:31:35 +0900

    chore: github desktop test
    test

--- src/main/java/sample/cafekiosk/spring/CafekioskApplication.java
+++ src/main/java/sample/cafekiosk/spring/CafekioskApplication.java
@@ -8,6 +8,6 @@ public class CafekioskApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CafekioskApplication.class, args);
+        // github desktop test
     }
-
 }
